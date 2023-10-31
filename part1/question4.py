54c50d4e9b47ebe888a17c4f548a03f203534f7a
restrepoTorres <vmanuel.restrepo@udea.edu.co>
2023-10-31 01:52:37 -0500

    feat: question4 solved

--- part1/question4.py
+++ part1/question4.py
@@ -23,9 +23,11 @@ import pets_db
 # The output should be a list of tuples in the format: (<pet name>, <species>, <age>)
 
 sql_pets_owned_by_nobody = """
-
-Your SQL here.
-
+SELECT 
+	name, species, age FROM animals 
+WHERE 
+	animal_id NOT IN (SELECT pet_id FROM people_animals)
+;
 """
 
 # Part 4.B:
@@ -33,16 +35,32 @@ Your SQL here.
 # The output should be an integer.
 
 sql_pets_older_than_owner = """
-
-Your SQL here.
-
+SELECT 
+	COUNT(pet_id) FROM people_animals 
+    LEFT JOIN 
+    	animals ON people_animals.pet_id = animals.animal_id
+    LEFT JOIN 
+    	people ON people_animals.owner_id = people.person_id
+WHERE 
+	(animals.age > people.age)
+;
 """
 
 # Part 4.C: BONUS CHALLENGE! 
 # Write SQL to select the pets that are owned by Bessie and nobody else.
 # The output should be a list of tuples in the format: (<person name>, <pet name>, <species>)
 sql_only_owned_by_bessie = """ 
-
-Your SQL here.
+SELECT people.name, animals.name, species
+FROM animals 
+JOIN people_animals ON animal_id = people_animals.pet_id
+JOIN people  ON people_animals.owner_id = people.person_id
+WHERE people.name = 'bessie'
+AND NOT EXISTS (
+    SELECT *
+    FROM people_animals 
+    JOIN people ON people_animals.owner_id = people.person_id
+    WHERE people_animals.pet_id = animals.animal_id
+    AND people.name <> 'bessie'
+);
 
 """
